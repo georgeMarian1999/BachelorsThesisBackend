2e506dfc17028dc2fb8f5aa35258a917f383f246
Marian George <50915367+georgeMarian1999@users.noreply.github.com>
2021-07-18 20:03:28 +0300

    Final commit

--- teme/src/index.ts
+++ teme/src/index.ts
@@ -1,24 +1,11 @@
-import express, { Request, Response } from 'express';
+import express from "express";
 import {json } from "body-parser";
-import { AxiosResponse } from 'axios';
+
 const app = express(); 
 app.use(json());
-var axios = require('axios');
-
-var config = {
-    /* Your settings here like Accept / Headers etc. */
-}
-
 
 
 app.get("/temelicenta",(req,res)=>{
-
-  axios.get('http://10.97.86.3:3500/users/crtuser')
-  .then(function(response: AxiosResponse){
-    console.log(response.data);
-  }).catch(function(err: Error){
-    console.log(err);
-  });
   res.send(
     [
     {
